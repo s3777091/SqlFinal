ab4fc727a4395a00c199956faaeff3791fbb50ff
Thu <hathu0610@gmail.com>
2023-09-06 13:29:34 +0700

    move code filter to shop

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 
 const db = require("../models");
-const User = db.user;
 
-const jwt = require("jsonwebtoken");
 const Product = db.product;
 const Category = db.category;
 
@@ -13,75 +11,41 @@ const Op = db.Sequelize.Op;
 
 
 require("dotenv").config();
-const config = require("../config/auth-config");
 
-const getSortBy = (sortBy) =>{
-  if(sortBy.includes("asc")){
-    return "ASC"
-  }
-  else if (sortBy.includes("desc")){
-    return "DESC"
-  }
-  else {
-    return "ASC"
-  }
-}
-
-const getSortKey = (sortBy) =>{
-  if(sortBy.includes("price")){
-    return "cost"
-  }
-  return "prName"
-}
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const transaction = await db.sequelize.transaction();
   try {
+
     const categories = await Category.findAll({
       attributes: ['id', 'name', 'image', 'slug', 'code', 'link'],
       raw: true,
       transaction
     });
-    if(typeof req.session === 'undefined' || Object.keys(req.session).length === 0){
-      await transaction.commit(); // Commit the transaction
-       return res.render('index', { title: 'Home Page', categories});
-    }
-    const decoded = jwt.verify(req.session.token, config.secret);
-    const user = await User.findByPk(decoded.id);
-    if (!user) {
-      await transaction.commit(); // Commit the transaction
-      return res.render('index', { title: 'Home Page', categories});
-    }
-    const userRoles = await user.getRoles();
-    const userInfo = {
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      roles: userRoles.map(role => role.name)
-    };
+
     await transaction.commit(); // Commit the transaction
-    return res.render('index', { title: 'Home Page', categories ,userInfo:userInfo});
-  }
-   catch (error) {
+
+    res.render('index', { title: 'Home Page', categories });
+  } catch (error) {
     await transaction.rollback(); // Rollback the transaction on error
     next(error); // Pass the error to the next middleware
   }
 });
 
 router.get('/filter', async (req, res, next) => {
+
   const transaction = await db.sequelize.transaction();
   try {
     const categoryId = req.query.categoryId;
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 6;
-    const searchTerm = req.query.search || "";
-    const sortBy = req.query.sortBy || "";
+    const limit = parseInt(req.query.limit) || 12;
     const offset = (page - 1) * limit;
-    console.log(page)
+
     if (!categoryId) {
       await transaction.rollback();
       return res.status(400).send({ message: "In-valid category ID." });
     }
+
     const category = await Category.findOne({
       where: {
         id: categoryId,
@@ -90,52 +54,26 @@ router.get('/filter', async (req, res, next) => {
       raw: true,
       transaction
     });
+
     if (!category) {
       await transaction.rollback();
       return res.status(404).send({ message: "Category not found." });
     }
-    var products = [];
-    if(searchTerm){
-      products = await Product.findAndCountAll({
-        where: {
-          [Op.or]: [
-            { prName: { [Op.like]: `%${searchTerm}%`} },
-          ],
-          categoryId: category.id,
-        },
-        attributes: ['id', 'prName', 'prId', 'prLink', 'image', 'cost'],
-        raw: true,
-        offset: parseInt(offset),
-        limit: limit,
-        order: [
-          [getSortKey(sortBy), getSortBy(sortBy)],
-      ],
-        transaction
-      });
-    }
-    else{
-      products = await Product.findAndCountAll({
-        where: {
-          categoryId: category.id,
-        },
-        attributes: ['id', 'prName', 'prId', 'prLink', 'image', 'cost'],
-        raw: true,
-        offset: parseInt(offset),
-        limit: limit,
-        order: [
-          [getSortKey(sortBy), getSortBy(sortBy)],
-      ],
-        transaction
-      });
-    }
-    console.log(products)
-    console.log(offset)
-    console.log(limit)
-    var totalPage = Math.ceil(products.count / limit);
+
+    const products = await Product.findAll({
+      where: {
+        categoryId: category.id
+      },
+      attributes: ['id', 'prName', 'prId', 'prLink', 'image', 'cost'],
+      raw: true,
+      offset: parseInt(offset),
+      limit: limit,
+      transaction
+    });
+
     const categoryWithProducts = {
       category_info: category,
-      products: products,
-      totalPage:totalPage
+      products: products
     };
 
     await transaction.commit();
@@ -151,10 +89,11 @@ router.post("/search", async (req, res, next) => {
     const searchTerm = req.body.search;
     const page = req.query.page || 1;
     const pageSize = 10;
+
     const products = await Product.findAndCountAll({
       where: {
         [Op.or]: [
-          { prName: { [Op.like]: "Beef" } },
+          { prName: { [Op.like]: `%${searchTerm}%` } },
         ],
       },
       // Implement pagination using offset and limit
@@ -169,6 +108,7 @@ router.post("/search", async (req, res, next) => {
       // Order by Full-Text Search score
       order: [[db.sequelize.literal("score"), "DESC"]],
     });
+
     res.status(200).json(products);
   } catch (error) {
     res.status(500).send({ message: error.message });
